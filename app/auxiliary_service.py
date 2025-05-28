560861523ce5b58282274aecc964517a971a16e4
Anhelina Zelyk <anhelina.zelyk@partner.bmwgroup.com>
2025-05-28 21:55:09 +0200

    Create main and auxiliary application

--- app/auxiliary_service.py
+++ app/auxiliary_service.py
@@ -1,1 +1,31 @@
-#placeholder
+from fastapi import FastAPI
+import boto3
+import os
+
+app = FastAPI()
+
+region = os.getenv("AWS_DEFAULT_REGION", "eu-central-1")
+
+s3_client = boto3.client("s3", region_name=region)
+ssm_client = boto3.client("ssm", region_name=region)
+
+AUX_SERVICE_VERSION = "1.0.0"
+
+@app.get("/s3-buckets")
+def list_s3_buckets():
+    buckets = s3_client.list_buckets()["Buckets"]
+    bucket_names = [bucket["Name"] for bucket in buckets]
+    return {"buckets": bucket_names, "version": AUX_SERVICE_VERSION}
+
+@app.get("/parameters")
+def list_parameters():
+    parameters = ssm_client.describe_parameters()["Parameters"]
+    param_names = [param["Name"] for param in parameters]
+    return {"parameters": param_names, "version": AUX_SERVICE_VERSION}
+
+@app.get("/parameter/{param_name:path}")
+def get_parameter(param_name: str):
+    if not param_name.startswith("/"):
+        param_name = "/" + param_name
+    response = ssm_client.get_parameter(Name=param_name)
+    return {"parameter": response["Parameter"]["Value"], "version": AUX_SERVICE_VERSION}
--- app/main_api.py
+++ app/main_api.py
@@ -1,30 +1,36 @@
-#placeholder
 from fastapi import FastAPI
-import boto3
 import os
+import requests
 
 app = FastAPI()
 
-# AWS Client
-s3_client = boto3.client("s3")
-ssm_client = boto3.client("ssm")
+AUX_SERVICE_URL = os.getenv("AUX_SERVICE_URL", "http://localhost:8001")
 
-# API version
-AUX_SERVICE_VERSION = "1.0.0"
+MAIN_API_VERSION = "1.0.0"
 
 @app.get("/s3-buckets")
 def list_s3_buckets():
-    buckets = s3_client.list_buckets()["Buckets"]
-    bucket_names = [bucket["Name"] for bucket in buckets]
-    return {"buckets": bucket_names, "version": AUX_SERVICE_VERSION}
+    response = requests.get(f"{AUX_SERVICE_URL}/s3-buckets").json()
+    return {
+        "buckets": response["buckets"],
+        "main_version": MAIN_API_VERSION,
+        "aux_version": response["version"]
+    }
 
 @app.get("/parameters")
 def list_parameters():
-    parameters = ssm_client.describe_parameters()["Parameters"]
-    param_names = [param["Name"] for param in parameters]
-    return {"parameters": param_names, "version": AUX_SERVICE_VERSION}
+    response = requests.get(f"{AUX_SERVICE_URL}/parameters").json()
+    return {
+        "parameters": response["parameters"],
+        "main_version": MAIN_API_VERSION,
+        "aux_version": response["version"]
+    }
 
-@app.get("/parameters/{param_name}")
+@app.get("/parameter/{param_name:path}")
 def get_parameter(param_name: str):
-    response = ssm_client.get_parameter(Name=param_name)
-    return {"parameter": response["Parameter"]["Value"], "version": AUX_SERVICE_VERSION}
+    response = requests.get(f"{AUX_SERVICE_URL}/parameter/{param_name}").json()
+    return {
+        "parameter": response["parameter"],
+        "main_version": MAIN_API_VERSION,
+        "aux_version": response["version"]
+    }
